01aa3414393dc6fbba7b4050a81d0093cc5d32ac
exw599 <cgu599@outlook.com>
2017-11-06 15:07:05 +0000

    Update parameter.hpp

--- src/parameter.hpp
+++ src/parameter.hpp
@@ -1,3 +1,17 @@
+/*----------------------------------------------------------------------
+ *  
+ *   	FIPI - Fast Interface Particle Interactions
+ *    
+ *     	https://bottogroup.wordpress.com/
+ *      
+ *     	Chuan Gu, c.gu@qmul.ac.uk
+ *     
+ *    	Copyright (2016) Botto Research Group
+ *  
+ * 	This software is distributed under the GNU General Public License.
+ * 
+ * ------------------------------------------------------------------------- */
+
 #ifndef PARAMETER_HPP
 #define PARAMETER_HPP
 
